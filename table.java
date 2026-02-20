97ad6c58fff1b8a85b093a7fd7ec121322e215b2
S.Mohanapriya <smohanapriya112233@gmail.com>
2026-02-20 21:23:06 +0530

    Create table.java

---
+++ table.java
@@ -0,0 +1,31 @@
+// multplication table any number
+
+import java.util.Scanner;
+class table
+{
+public static void main(String[] args)
+{
+Scanner scan=new Scanner(System.in);
+System.out.print("enter the number:");
+int num=scan.nextInt();
+int n=10;
+for(int i=1;i<=n;i++)
+{
+System.out.println(i+"x"+"num"+"="+i*n);
+}
+
+
+
+
+// multiplication table for specific number
+
+import java.util.Scanner;
+class table
+{
+public static void main(String[] args)
+{
+for(int i=1;i<=10;i++)
+{
+System.out.println(i+"x5"+="+i*5);
+}
+
