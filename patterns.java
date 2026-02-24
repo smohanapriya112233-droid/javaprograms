c0289bb79e577eec31f671c045b2e63b19705ef4
S.Mohanapriya <smohanapriya112233@gmail.com>
2026-02-24 17:10:41 +0530

    Update patterns.java

--- patterns.java
+++ patterns.java
@@ -21,4 +21,28 @@ class pat
 }
 }
 
+/* 2)
+*
+**
+***
+****
+*****  */
+
+import java.util.Scanner;
+class pat
+{
+    public static void main(String[] args)
+    {
+    int a=5;
+    for(int i=1;i<=a;i++)
+    {
+        for(int j=1;j<=i;j++)
+        {
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+    }
+}
+
 
